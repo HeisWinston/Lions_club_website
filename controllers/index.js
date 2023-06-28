81d5bd2a29d4352e5bb3c88c2c17b28a79cae10d
Winston RIley <wriley2018@knights.ucf.edu>
2023-06-28 19:42:12 -0400

    added handle bars to merch code

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const home = require('./home');
 const events = require('./events');
+const merch = require('./merch');
+
 
 router.use('/', home);
 router.use('/events', events);
+router.use('/merch', merch);
+
 
 
 module.exports = router;
