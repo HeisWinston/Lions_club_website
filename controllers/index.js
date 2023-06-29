d5e261dc23fde71766f0115b19a485a87472ebec
Winston RIley <wriley2018@knights.ucf.edu>
2023-06-28 21:07:00 -0400

    making some login progress

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const home = require('./home');
 const events = require('./events');
 const merch = require('./merch');
+const sign_up = require('./sign_up');
 
 
 router.use('/', home);
 router.use('/events', events);
 router.use('/merch', merch);
+router.use('/sign_up', sign_up);
 
 
 
---
+++ controllers/sign_up.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+    res.render('homepage', { layout: 'sign_up', logged_in: req.session.logged_in });
+})
+
+
+module.exports = router;
