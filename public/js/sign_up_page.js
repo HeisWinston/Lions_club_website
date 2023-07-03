3f3cb5b94df03102ddf6f43895e56f1e301c48f4
Winston RIley <wriley2018@knights.ucf.edu>
2023-07-03 18:45:56 -0400

    finished sign up and login front end pretty much

--- public/js/sign_up_page.js
+++ public/js/sign_up_page.js
@@ -1,10 +1,30 @@
 const forms = document.querySelector(".forms"),
-        pwShowHide = document.querySelector(".eye-icon"),
-        links  = document.querySelector(".link");
+        pwShowHide = document.querySelectorAll(".eye-icon"),
+        links  = document.querySelectorAll(".link");
+console.log(forms,pwShowHide,links )
 
-pwShowHide.forEeach(eyeIcon => {
-        eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentELement.parentELement.querySelectorAll(".password");
-        console.log(pwFields)
+pwShowHide.forEach(eyeIcon => {
+        eyeIcon.addEventListener("click",() => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+      
+        
+        pwFields.forEach(password => {
+                if(password.type === "password"){
+                        password.type = "text";
+                        eyeIcon.classList.replace("bx-hode", "bx-show");
+                        return; 
+
+                }
+                password.type = "password";
+                        eyeIcon.classList.replace("bx-hode", "bx-show");
+        })
     })
+})
+
+links.forEach(link =>{
+        link.addEventListener("click",e => {
+                e.preventDefault(); 
+                forms.classList.toggle("show-signup");
+
+        })
 })
