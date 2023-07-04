452cee7a21bdf8fc6efd5368438c5fbf5c96b77c
Winston RIley <wriley2018@knights.ucf.edu>
2023-07-04 19:18:24 -0400

    user profiles started

--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,14 @@ const home = require('./home');
 const events = require('./events');
 const merch = require('./merch');
 const sign_up = require('./sign_up');
+const forum = require('./forum');
 
 
 router.use('/', home);
 router.use('/events', events);
 router.use('/merch', merch);
 router.use('/sign_up', sign_up);
+router.use('/forum', forum);
 
 
 
