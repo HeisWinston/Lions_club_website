eb1759901bf69964b0bd6dd4a1b977a790099303
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2023-07-16 13:52:06 -0400

    home page changes

--- controllers/post_login_home.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', async (req, res) => {
-    res.render('homepage', { layout: 'post_login_home', logged_in: req.session.logged_in });
-})
-
-
-module.exports = router;
