e30383c1b205cff38eb347f407bb4aed95f4016c
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2023-07-16 13:43:28 -0400

    removed post login route

--- controllers/index.js
+++ controllers/index.js
@@ -4,16 +4,12 @@ const events = require('./events');
 const merch = require('./merch');
 const user = require('./user');
 const forum = require('./forum');
-const post_login_home = require('./post_login_home');
 
 // fixing merge conflict
 router.use('/', home);
 router.use('/events', events);
 router.use('/merch', merch);
-router.use('/sign_up', sign_up);
-router.use('/post_login_home', post_login_home);
-
-
+router.use('/user', user);
 
 
 module.exports = router;
